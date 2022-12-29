56e8cbc129251697709bae9457aef30e314a33a4
Chris Waters <bieoe.ffe@gmail.com>
2022-12-29 11:10:17 -0600

    Commented logger.example.js, added more demo code

--- src/logger/logger.example.js
+++ src/logger/logger.example.js
@@ -1,11 +1,22 @@
 import { Logger, LOG_LEVELS, getLogger, deleteLogger } from './logger.js'
 
+/**
+ * Use getLogger and deleteLogger for managed logger instances
+ */
 const loggerName = 'myLogger'
 const logger = getLogger(loggerName)
-logger.debug('This won\'t be logger')
-logger.info('This will be logged')
+logger.debug('logger.debug: this won\'t be logger')
+logger.info('logger.info: This will be logged')
+logger.setLevel(LOG_LEVELS.debug)
+logger.debug('logger.debug: This will now be logged')
 deleteLogger(loggerName)
+const newLogger = getLogger(loggerName)
+newLogger.debug('logger.debug: new logger instance, this won\'t be logged')
 
+/**
+ * Create your own logger
+ * NOTE: This means you are responsible for managing logger instance (e.g., need to pass to functions to use same logger instance)
+ */
 const myLogger = new Logger(LOG_LEVELS.debug)
-myLogger.verbose('This won\'t be logged')
-myLogger.debug('This will be logged')
+myLogger.verbose('logger.verbose: This won\'t be logged')
+myLogger.debug('logger.debug: This will be logged')
