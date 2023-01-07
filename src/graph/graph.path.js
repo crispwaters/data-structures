25ba5d8f196bc2ecc1e22b372d0ad66601fb0be2
Chris Waters <bieoe.ffe@gmail.com>
2023-01-07 16:58:13 -0600

    Implemented Dijkstra, slight refactor, fixed bug where DFS altered vertex edges

--- src/graph/graph.path.js
+++ src/graph/graph.path.js
@@ -1,4 +1,5 @@
 import { Vertex } from './graph.vertex.js' // eslint-disable-line no-unused-vars -- Importing for JSDoc
+import { Edge } from './graph.edge.js' // eslint-disable-line no-unused-vars -- Importing for JSDoc
 import { LinkedList } from '../linked-list/linked-list.js'
 
 export class Path {
@@ -30,8 +31,8 @@ export class Path {
   bfs () {
     const globalVisited = new Set()
     const q = new LinkedList([{ current: this.start, visited: new Set(globalVisited) }])
-    return this.search(
-      () => {
+    return this.search({
+      hasNext: () => {
         while (q.getCount() > 0) {
           if (!globalVisited.has(q.first().current)) {
             return true
@@ -40,19 +41,20 @@ export class Path {
         }
         return false
       },
-      () => {
+      getVertex: () => {
+        /** @type {{ current: Vertex, visited: Set<Vertex> }} */
         const { current, visited } = q.dequeue()
         globalVisited.add(current)
         visited.add(current)
         return { current, visited }
       },
-      ({ current: { edges }, visited }) => {
+      addVerticies: ({ current: { edges }, visited }) => {
         for (const { target } of edges) {
           if (globalVisited.has(target)) continue
           q.enqueue({ current: target, visited: new Set(visited) })
         }
       }
-    )
+    })
   }
 
   /**
@@ -62,8 +64,8 @@ export class Path {
   dfs () {
     const globalVisited = new Set()
     const s = new LinkedList([{ current: this.start, visited: new Set(globalVisited) }])
-    return this.search(
-      () => {
+    return this.search({
+      hasNext: () => {
         while (s.getCount() > 0) {
           if (!globalVisited.has(s.peek().current)) {
             return true
@@ -72,31 +74,80 @@ export class Path {
         }
         return false
       },
-      () => {
+      getVertex: () => {
+        /** @type {{ current: Vertex, visited: Set<Vertex> }} */
         const { current, visited } = s.pop()
         globalVisited.add(current)
         visited.add(current)
         return { current, visited }
       },
-      ({ current: { edges }, visited }) => {
-        while (edges.length) {
-          const { target } = edges.pop()
+      addVerticies: ({ current: { edges }, visited }) => {
+        for (let i = edges.length - 1; i >= 0; i--) {
+          const { target } = edges[i]
           if (globalVisited.has(target)) continue
           s.push({ current: target, visited: new Set(visited) })
         }
       }
-    )
+    })
+  }
+
+  /**
+   * Traverse grid via Dijkstra's Algorith to find a path with the shortest distance
+   * @returns {Path} Path containing route found via Dijkstra's Algorithm
+   */
+  dijkstra () {
+    const globalVisited = new Set([this.start])
+    const pq = new LinkedList()
+    const distances = new Map()
+    distances.set(this.start, 0)
+    const getTotalWeight =
+    /**
+     * @param {Vertex} from
+     * @param {Edge} edge
+     * @returns {number}
+     */
+    (from, edge) => distances.get(from) + edge.weight
+    for (const edge of this.start.edges) {
+      distances.set(edge, edge.weight)
+      pq.addMinPriority({ current: edge.target, edge, from: this.start, visited: new Set(globalVisited) }, edge.weight)
+    }
+    return this.search({
+      hasNext: () => {
+        while (pq.getCount() > 0) {
+          if (!globalVisited.has(pq.first().current)) {
+            return true
+          }
+          pq.dequeue()
+        }
+      },
+      getVertex: () => {
+        const cur = pq.dequeue()
+        const { current, visited, edge, from } = cur
+        globalVisited.add(current)
+        visited.add(current)
+        distances.set(current, getTotalWeight(from, edge))
+        return cur
+      },
+      addVerticies: ({ current, visited }) => {
+        for (const edge of current.edges) {
+          if (globalVisited.has(edge.target)) continue
+          pq.addMinPriority({ current: edge.target, edge, from: current, visited: new Set(visited) }, getTotalWeight(current, edge))
+        }
+      }
+    })
   }
 
   /**
    * Abstract search implementation
-   * @param {() => boolean} hasNext true if there is another vertex to visit, false otherwise
-   * @param {() => { current: Vertex, visited: Set<Vertex> }} getVertex get next vertex to visit
-   * @param {({ current: Vertex, visited: Set<Vertex> }) => void} addVerticies add verticies to collection of possible verticies to visit
+   * @param {object} funcs
+   * @param {() => boolean} funcs.hasNext true if there is another vertex to visit, false otherwise
+   * @param {() => { current: Vertex, visited: Set<Vertex> }} funcs.getVertex get next vertex to visit
+   * @param {(params: { current: Vertex, visited: Set<Vertex> }) => void} funcs.addVerticies add verticies to collection of possible verticies to visit
    */
-  search (hasNext, getVertex, addVerticies) {
+  search ({ hasNext, getVertex, addVerticies }) {
     while (hasNext()) {
-      const { current, visited } = getVertex()
+      const cur = getVertex()
+      const { current, visited } = cur
       if (current === this.finish) {
         const path = new Path(this.start, this.finish)
         path.visited = visited
@@ -104,7 +155,7 @@ export class Path {
         path.isTerminated = true
         return path
       }
-      addVerticies({ current, visited })
+      addVerticies(cur)
     }
     const path = new Path(this.start, this.finish)
     path.isTerminated = true
--- src/graph/graph.path.test.js
+++ src/graph/graph.path.test.js
@@ -63,6 +63,42 @@ test('BFS marks path not found when no path exists to target', t => {
   t.true(path.isTerminated)
 })
 
+test('BFS does not alter the graph', t => {
+  // #region setup
+  const start = new Vertex()
+  const finish = new Vertex()
+  const a = new Vertex()
+  const b = new Vertex()
+  const c = new Vertex()
+
+  // #region edges
+  start.addEdge({ target: a })
+  start.addEdge({ target: b })
+  a.addEdge({ target: finish })
+  a.addEdge({ target: c })
+  b.addEdge({ target: a })
+  b.addEdge({ target: c })
+  c.addEdge({ target: a })
+  c.addEdge({ target: finish })
+  // #endregion
+  // #endregion
+
+  new Path(start, finish).bfs()
+  t.is(start.edges.length, 2)
+  t.true(start.edges.some(({ target }) => target === a))
+  t.true(start.edges.some(({ target }) => target === b))
+  t.is(a.edges.length, 2)
+  t.true(a.edges.some(({ target }) => target === finish))
+  t.true(a.edges.some(({ target }) => target === c))
+  t.is(b.edges.length, 2)
+  t.true(b.edges.some(({ target }) => target === a))
+  t.true(b.edges.some(({ target }) => target === c))
+  t.is(b.edges.length, 2)
+  t.true(c.edges.some(({ target }) => target === a))
+  t.true(c.edges.some(({ target }) => target === finish))
+  t.is(finish.edges.length, 0)
+})
+
 test('DFS returns path', t => {
   // #region setup
   const start = new Vertex()
@@ -134,4 +170,140 @@ test('DFS marks path not found when no path exists to target', t => {
   const path = new Path(start, finish).dfs()
   t.false(path.isPathFound)
   t.true(path.isTerminated)
+})
+
+test('DFS does not alter the graph', t => {
+  // #region setup
+  const start = new Vertex()
+  const finish = new Vertex()
+  const a = new Vertex()
+  const b = new Vertex()
+  const c = new Vertex()
+  const d = new Vertex()
+  const e = new Vertex()
+  const f = new Vertex()
+  const g = new Vertex()
+  const h = new Vertex()
+
+  // #region edges
+  start.addEdge({ target: a })
+  start.addEdge({ target: b })
+  a.addEdge({ target: g })
+  a.addEdge({ target: h })
+  a.addEdge({ target: b })
+  a.addEdge({ target: d })
+  a.addEdge({ target: f })
+  b.addEdge({ target: a })
+  b.addEdge({ target: c })
+  c.addEdge({ target: d })
+  c.addEdge({ target: f })
+  d.addEdge({ target: e })
+  e.addEdge({ target: f })
+  f.addEdge({ target: finish })
+  g.addEdge({ target: h })
+  // #endregion
+  // #endregion
+
+  new Path(start, finish).dfs()
+  t.is(start.edges.length, 2)
+  t.true(start.edges.some(({ target }) => target === a))
+  t.true(start.edges.some(({ target }) => target === b))
+  t.is(a.edges.length, 5)
+  t.true(a.edges.some(({ target }) => target === g))
+  t.true(a.edges.some(({ target }) => target === h))
+  t.true(a.edges.some(({ target }) => target === b))
+  t.true(a.edges.some(({ target }) => target === d))
+  t.true(a.edges.some(({ target }) => target === f))
+  t.is(b.edges.length, 2)
+  t.true(b.edges.some(({ target }) => target === a))
+  t.true(b.edges.some(({ target }) => target === c))
+  t.is(c.edges.length, 2)
+  t.true(c.edges.some(({ target }) => target === d))
+  t.true(c.edges.some(({ target }) => target === f))
+  t.is(d.edges.length, 1)
+  t.true(d.edges.some(({ target }) => target === e))
+  t.is(e.edges.length, 1)
+  t.true(e.edges.some(({ target }) => target === f))
+  t.is(f.edges.length, 1)
+  t.true(f.edges.some(({ target }) => target === finish))
+  t.is(g.edges.length, 1)
+  t.true(g.edges.some(({ target }) => target === h))
+  t.is(h.edges.length, 0)
+})
+
+test('Dijkstra finds shortest path on weighted graph', t => {
+  // #region setup
+  const start = new Vertex()
+  const finish = new Vertex()
+  const a = new Vertex()
+  const b = new Vertex()
+  const c = new Vertex()
+  const d = new Vertex()
+
+  // #region edges
+  start.addEdge({ target: a, weight: 5 })
+  start.addEdge({ target: b, weight: 1 })
+  a.addEdge({ target: finish, weight: 10 })
+  b.addEdge({ target: c, weight: 2 })
+  b.addEdge({ target: a, weight: 3 })
+  c.addEdge({ target: d, weight: 3 })
+  d.addEdge({ target: finish, weight: 4 })
+  d.addEdge({ target: a, weight: 1 })
+  // #endregion
+
+  const expected = new Set([start, b, c, d, finish])
+  // #endregion
+
+  const path = new Path(start, finish).dijkstra()
+  t.true(path.isPathFound)
+  t.true(path.isTerminated)
+  const iExpected = expected.values()
+  const iActual = path.visited.values()
+  let rExpected = iExpected.next()
+  let rActual = iActual.next()
+  while (!rExpected.done && !rActual.done) {
+    t.is(rActual.value, rExpected.value)
+    rExpected = iExpected.next()
+    rActual = iActual.next()
+  }
+  t.true(rExpected.done, rExpected.value)
+  t.true(rActual.done, rActual.value)
+})
+
+test('Dijkstra does not alter the graph', t => {
+  // #region setup
+  const start = new Vertex()
+  const finish = new Vertex()
+  const a = new Vertex()
+  const b = new Vertex()
+  const c = new Vertex()
+  const d = new Vertex()
+
+  // #region edges
+  start.addEdge({ target: a, weight: 5 })
+  start.addEdge({ target: b, weight: 1 })
+  a.addEdge({ target: finish, weight: 10 })
+  b.addEdge({ target: c, weight: 2 })
+  b.addEdge({ target: a, weight: 3 })
+  c.addEdge({ target: d, weight: 3 })
+  d.addEdge({ target: finish, weight: 4 })
+  d.addEdge({ target: a, weight: 1 })
+  // #endregion
+  // #endregion
+
+  new Path(start, finish).dijkstra()
+  t.is(start.edges.length, 2)
+  t.true(start.edges.some(({ target }) => target === a))
+  t.true(start.edges.some(({ target }) => target === b))
+  t.is(a.edges.length, 1)
+  t.true(a.edges.some(({ target }) => target === finish))
+  t.is(b.edges.length, 2)
+  t.true(b.edges.some(({ target }) => target === a))
+  t.true(b.edges.some(({ target }) => target === c))
+  t.is(c.edges.length, 1)
+  t.true(c.edges.some(({ target }) => target === d))
+  t.is(d.edges.length, 2)
+  t.true(d.edges.some(({ target }) => target === a))
+  t.true(d.edges.some(({ target }) => target === finish))
+  t.is(finish.edges.length, 0)
 })
